8daf1a71f0fbac3e92f79b9160c6500dccb53048
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-06-11 16:50:56 +0100

    |BUILD| [build-gcc-final.sh] * Before building, the temporary toolchain's path is added to PATH, allowing   the build to succeed
    |NOTES|
    [README]
    * Added a basic README

--- build-gcc-final.sh
+++ build-gcc-final.sh
@@ -6,6 +6,8 @@ set -e
 mkdir $BUILDDIR/gcc-final
 cd $BUILDDIR/gcc-final
 
+export PATH=$INSTALLDIR/bin:$PATH
+
 $SRCDIR/gcc-3.4.6/configure --prefix=$INSTALLDIR --target=$TARGETMACH --enable-languages=c --disable-shared --disable-threads --disable-multilib --disable-libmudflap --with-newlib --program-prefix=${PROGRAM_PREFIX}
 
 make
