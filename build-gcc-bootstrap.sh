fd7575fd1aba1b5bd75f3e77fdb4a1c72181941e
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-06-16 15:28:31 +0100

    |GCC| * Fixed a typing error when checking for the GCC bootstrap build directory * Both the final and bootstrap versions of GCC use --with-cpu=m2

--- build-gcc-bootstrap.sh
+++ build-gcc-bootstrap.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 set -e
 
-
 export NCPU=`nproc`
 
-[ -d $BUILDIDR/gcc-bootstrap ] && rm -rf $BUILDDIR/gcc-bootstrap
+[ -d $BUILDDIR/gcc-bootstrap ] && rm -rf $BUILDDIR/gcc-bootstrap
 
 mkdir -p $BUILDDIR/gcc-bootstrap
 cd $BUILDDIR/gcc-bootstrap
 
 PATH=$INSTALLDIR/bin:$PATH
 
-$SRCDIR/gcc-4.9.0/configure --build=$BUILDMACH --host=$BUILDMACH --target=$TARGETMACH --prefix=$INSTALLDIR --without-headers --enable-bootstrap --enable-languages=c --disable-threads --disable-libmudflap --with-gnu-ld --with-gnu-as --disable-libssp --disable-libgomp --disable-nls --disable-shared --program-prefix=${PROGRAM_PREFIX} --with-newlib
+$SRCDIR/gcc-4.9.0/configure --build=$BUILDMACH --host=$BUILDMACH --target=$TARGETMACH --prefix=$INSTALLDIR --without-headers --enable-bootstrap --enable-languages=c --disable-threads --disable-libmudflap --with-gnu-ld --with-gnu-as --disable-libssp --disable-libgomp --disable-nls --disable-shared --program-prefix=${PROGRAM_PREFIX} --with-newlib --with-cpu=m2
 
 make all-gcc -j${NCPU}
 make install-gcc -j${NCPU}
